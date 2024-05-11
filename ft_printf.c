6442e108776ceec1bd8c84b80d3ba39a875e0e94
Andres Genaro Bastardo Freitas <abastard@c1r7s2.42madrid.com>
2024-05-11 17:31:33 +0200

    may 11

---
+++ ft_printf.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abastard <abastard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/11 15:13:40 by abastard          #+#    #+#             */
+/*   Updated: 2024/05/11 16:41:42 by abastard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_printf(char const *str, ...)
+{
+    va_list va;
+    size_t counter;
+    
+    if (!str)
+        return (0);
+    counter = 0;
+    va_start(va, str)
+    while (*str)
+    {
+        if (*str == '%')
+        {
+            str++;
+            ft_ (va, (char *)str, &counter);
+        }
+        else
+            ft_putcharf(*str,&counter);
+        str++;
+    }
+    va_end(va);
+    return (counter);
+}
+
+void    ft_format(va_list va, char *str, size_t *counter)
+{
+    if (*str =='c') //character
+        ft_
+    else if (*str =='s') //string
+        ft_
+    else if (*str =='p') //puntero
+        ft_
+    else if (*str =='i' || *str =='d') //numero
+        ft_
+    else if (*str =='u') //size_t
+        ft_
+    else if (*str =='x') // char to in
+        ft_
+    else if (*str =='%') // %
+        ft_
+}
